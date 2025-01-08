bd5fdc1463b4cd95899584e33d23c9ccf9bec3bb
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2025-01-07 19:54:51 -0800

    Rework updater

--- src/main/java/io/wdsj/hybridfix/command/CommandHybridFix.java
+++ src/main/java/io/wdsj/hybridfix/command/CommandHybridFix.java
@@ -1,19 +1,31 @@
 package io.wdsj.hybridfix.command;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import io.wdsj.hybridfix.HybridFix;
+import io.wdsj.hybridfix.config.Settings;
 import io.wdsj.hybridfix.util.ItemStackUtils;
 import io.wdsj.hybridfix.util.SpigotReflectionUtils;
+import io.wdsj.hybridfix.util.Updater;
+import io.wdsj.hybridfix.util.Utils;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.minecraft.item.ItemStack;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.Locale;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class CommandHybridFix extends Command {
+    private static final Cache<String, String> versionCache = CacheBuilder.newBuilder()
+            .expireAfterWrite(1L, TimeUnit.HOURS)
+            .maximumSize(1L)
+            .build();
     public CommandHybridFix(String name) {
         super(name);
         this.description = "HybridFix commands";
@@ -48,7 +60,34 @@ public class CommandHybridFix extends Command {
                 sender.spigot().sendMessage(message);
                 break;
             case "version":
-                sender.sendMessage("This server is running HybridFix version " + HybridFix.VERSION);
+                sender.sendMessage("This server is running HybridFix version " + HybridFix.VERSION + " (" + Bukkit.getVersion() + ")");
+                if (Settings.checkForUpdates) {
+                    sender.sendMessage(ChatColor.ITALIC + "Checking version, please wait...");
+                    String cachedLatestVersion = versionCache.getIfPresent(HybridFix.VERSION);
+                    if (cachedLatestVersion != null) {
+                        if (!cachedLatestVersion.equals(HybridFix.VERSION)) {
+                            sender.sendMessage(ChatColor.YELLOW + "* There is an update available: " + Updater.getLatestVersion() + ", you're on: " + HybridFix.VERSION + ".");
+                        } else {
+                            sender.sendMessage(ChatColor.GREEN + "* You are running the latest version.");
+                        }
+                    } else {
+                        CompletableFuture.supplyAsync(Updater::isUpdateAvailable, Utils.ioWorker())
+                                .thenAccept(
+                                        isUpdateAvailable -> {
+                                            if (isUpdateAvailable) {
+                                                sender.sendMessage(ChatColor.YELLOW + "* There is an update available: " + Updater.getLatestVersion() + ", you're on: " + HybridFix.VERSION + ".");
+                                            } else {
+                                                if (!Updater.isErred()) {
+                                                    sender.sendMessage(ChatColor.GREEN + "* You are running the latest version.");
+                                                } else {
+                                                    sender.sendMessage(ChatColor.RED + "* Error obtaining version information.");
+                                                }
+                                            }
+                                            versionCache.put(HybridFix.VERSION, Updater.getLatestVersion());
+                                        }
+                                );
+                    }
+                }
                 break;
             default:
                 sender.sendMessage(ChatColor.RED + "Usage: " + usageMessage);
