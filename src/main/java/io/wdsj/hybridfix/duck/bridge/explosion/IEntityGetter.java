fab110011bdb321c733e5bef4160c590988cbab0
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2025-01-22 00:11:18 -0800

    Default method to avoid crappy hybrid implementations

--- src/main/java/io/wdsj/hybridfix/duck/bridge/explosion/IEntityGetter.java
+++ src/main/java/io/wdsj/hybridfix/duck/bridge/explosion/IEntityGetter.java
@@ -3,5 +3,7 @@ package io.wdsj.hybridfix.duck.bridge.explosion;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftEntity;
 
 public interface IEntityGetter {
-    CraftEntity getBukkitEntity();
+    default CraftEntity getBukkitEntity() {
+        throw new AssertionError("Not implemented");
+    }
 }
