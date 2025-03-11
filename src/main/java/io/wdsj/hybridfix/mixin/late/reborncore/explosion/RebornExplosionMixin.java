45964d0e87a04a54b1590f27dd16b4f351ed4751
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2025-03-12 04:37:46 +1400

    Move block calculation to explode

--- src/main/java/io/wdsj/hybridfix/mixin/late/reborncore/explosion/RebornExplosionMixin.java
+++ src/main/java/io/wdsj/hybridfix/mixin/late/reborncore/explosion/RebornExplosionMixin.java
@@ -11,6 +11,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.ExplosionEvent;
 import org.apache.commons.lang3.time.StopWatch;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -26,30 +27,14 @@ import java.util.List;
  */
 @Mixin(RebornExplosion.class)
 public abstract class RebornExplosionMixin extends Explosion {
+    @Shadow
+    BlockPos center;
+
+    @Shadow
+    int radius;
+
     public RebornExplosionMixin(World worldIn, Entity entityIn, double x, double y, double z, float size, List<BlockPos> affectedPositions) {
         super(worldIn, entityIn, x, y, z, size, affectedPositions);
-    }
-
-    @Inject(
-            method = "<init>",
-            at = @At("TAIL")
-    )
-    public void init(BlockPos center, World world, int radius, CallbackInfo ci) {
-        for (int tx = -radius; tx < radius + 1; tx++) {
-            for (int ty = -radius; ty < radius + 1; ty++) {
-                for (int tz = -radius; tz < radius + 1; tz++) {
-                    if (Math.sqrt(Math.pow(tx, 2) + Math.pow(ty, 2) + Math.pow(tz, 2)) <= radius - 2) {
-                        BlockPos pos = center.add(tx, ty, tz);
-                        IBlockState state = world.getBlockState(pos);
-                        Block block = state.getBlock();
-                        float hardness = state.getBlockHardness(world, pos);
-                        if (block != Blocks.BEDROCK && block != Blocks.AIR && hardness >= 0.0F && hardness < 50.0F) {
-                            affectedBlockPositions.add(pos);
-                        }
-                    }
-                }
-            }
-        }
     }
 
     @Inject(
@@ -66,15 +51,28 @@ public abstract class RebornExplosionMixin extends Explosion {
         }
         StopWatch watch = new StopWatch();
         watch.start();
+        for (int tx = -radius; tx < radius + 1; tx++) {
+            for (int ty = -radius; ty < radius + 1; ty++) {
+                for (int tz = -radius; tz < radius + 1; tz++) {
+                    if (Math.sqrt(Math.pow(tx, 2) + Math.pow(ty, 2) + Math.pow(tz, 2)) <= radius - 2) {
+                        BlockPos pos = center.add(tx, ty, tz);
+                        IBlockState state = world.getBlockState(pos);
+                        Block block = state.getBlock();
+                        float hardness = state.getBlockHardness(world, pos);
+                        if (block != Blocks.BEDROCK && block != Blocks.AIR && hardness >= 0.0F && hardness < 50.0F) {
+                            affectedBlockPositions.add(pos);
+                        }
+                    }
+                }
+            }
+        }
         ExplosionEvent.Detonate event = new ExplosionEvent.Detonate(this.world, this, new ArrayList<>());
         MinecraftForge.EVENT_BUS.post(event);
         for (BlockPos pos : affectedBlockPositions) {
             IBlockState state = this.world.getBlockState(pos);
             Block block = state.getBlock();
-            if (block != Blocks.BEDROCK && block != Blocks.AIR) {
-                block.onBlockExploded(this.world, pos, this);
-                this.world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
-            }
+            block.onBlockExploded(this.world, pos, this);
+            this.world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
         }
         RebornCore.logHelper.info("The explosion took " + watch + " to explode");
     }
