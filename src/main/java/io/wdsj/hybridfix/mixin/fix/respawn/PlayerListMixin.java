0644bd5e683c3606739868f1c0e7072bca41dcfd
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2025-04-26 05:41:49 +1400

    Improve readability of respawn fix

--- src/main/java/io/wdsj/hybridfix/mixin/fix/respawn/PlayerListMixin.java
+++ src/main/java/io/wdsj/hybridfix/mixin/fix/respawn/PlayerListMixin.java
@@ -33,10 +33,10 @@ public abstract class PlayerListMixin {
             ),
             remap = false
     )
-    public void onMoveToWorld(EntityPlayerMP playerIn, int i, boolean b, Location loc, boolean b1, CallbackInfoReturnable<EntityPlayerMP> cir, @Share("newCap") LocalRef<CapabilityDispatcher> newCap) {
+    public void onMoveToWorld(EntityPlayerMP playerIn, int dimensionId, boolean conqueredEnd, Location loc, boolean avoidSuffocation, CallbackInfoReturnable<EntityPlayerMP> cir, @Share("newCap") LocalRef<CapabilityDispatcher> newCap) {
         if (Settings.simulateVanillaRespawn) CBRespawnFixLogic.simulateVanillaRespawn(playerIn);
         if (Settings.fixCapabilityReset) {
-            newCap.set(ForgeEventFactory.gatherCapabilities(playerIn));
+            newCap.set(ForgeEventFactory.gatherCapabilities(playerIn)); // Prepare a new capability dispatcher
         }
     }
 
@@ -51,14 +51,14 @@ public abstract class PlayerListMixin {
             ),
             remap = false
     )
-    public void afterCopy(EntityPlayerMP player, int i, boolean b, Location loc, boolean b1, CallbackInfoReturnable<EntityPlayerMP> cir, @Share("newCap") LocalRef<CapabilityDispatcher> newCap) {
+    public void afterCopy(EntityPlayerMP player, int dimensionId, boolean conqueredEnd, Location loc, boolean avoidSuffocation, CallbackInfoReturnable<EntityPlayerMP> cir, @Share("newCap") LocalRef<CapabilityDispatcher> newCap) {
         if (Settings.fixCapabilityReset) {
-            FakePlayer dummy = Objects.requireNonNull(HybridFixFakePlayer.get(player.getEntityWorld(), player.getPosition()).get());
+            FakePlayer dummyPlayer = Objects.requireNonNull(HybridFixFakePlayer.get(player.getEntityWorld(), player.getPosition()).get());
             CapabilityDispatcher dispatcher = newCap.get();
-            ((EntityCapabilityAccessor) (Entity) dummy).setCapabilities(dispatcher);
-            ForgeEventFactory.onPlayerClone(dummy, player, !b);
-            CapabilityDispatcher newCapability = ((EntityCapabilityAccessor) (Entity) dummy).getCapabilities();
-            ((EntityCapabilityAccessor) (Entity) player).setCapabilities(newCapability);
+            ((EntityCapabilityAccessor) (Entity) dummyPlayer).setCapabilities(dispatcher); // Set the fake player's capabilities to the new dispatcher
+            ForgeEventFactory.onPlayerClone(dummyPlayer, player, !conqueredEnd); // Fire another event for the fake player
+            CapabilityDispatcher newCapability = ((EntityCapabilityAccessor) (Entity) dummyPlayer).getCapabilities();
+            ((EntityCapabilityAccessor) (Entity) player).setCapabilities(newCapability); // Copy the re-gathered CapabilityDispatcher to the actual player
         }
     }
 }
