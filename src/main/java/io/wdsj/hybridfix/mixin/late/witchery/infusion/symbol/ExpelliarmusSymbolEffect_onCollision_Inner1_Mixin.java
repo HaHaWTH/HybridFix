c30ce2ed8b99610acef1429a6d435f7b29191f2a
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2025-05-22 03:21:36 +1400

    rename mixin

--- src/main/java/io/wdsj/hybridfix/mixin/late/witchery/infusion/symbol/ExpelliarmusSymbolEffect_onCollision_Inner1_Mixin.java
+++ src/main/java/io/wdsj/hybridfix/mixin/late/witchery/infusion/symbol/ExpelliarmusSymbolEffect_onCollision_Inner1_Mixin.java
@@ -12,21 +12,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(targets = "net.msrandom.witchery.infusion.symbol.FlipendoSymbolEffect$onCollision$1", priority = 999)
-public abstract class FlipendoSymbolEffectMixin {
+@Mixin(targets = "net.msrandom.witchery.infusion.symbol.ExpelliarmusSymbolEffect$onCollision$1")
+public abstract class ExpelliarmusSymbolEffect_onCollision_Inner1_Mixin {
     @Dynamic
     @Inject(
             method = "accept(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/entity/EntityLivingBase;)V",
             at = @At(
-                    value = "CONSTANT",
-                    remap = true,
-                    args = "classValue=net/minecraft/entity/player/EntityPlayerMP",
-                    ordinal = 0
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/EntityLivingBase;entityDropItem(Lnet/minecraft/item/ItemStack;F)Lnet/minecraft/entity/item/EntityItem;",
+                    remap = true
             ),
             remap = false,
             cancellable = true
     )
-    public void beforeMove(EntityLivingBase actor, EntityLivingBase target, CallbackInfo ci) {
+    public void onEntityDropItem(EntityLivingBase actor, EntityLivingBase target, CallbackInfo ci) {
         Entity bActor = ((IEntityGetter) actor).getBukkitEntity();
         Entity bTarget = ((IEntityGetter) target).getBukkitEntity();
         // noinspection deprecation
