e33408250dbcafa5381102e88661f1fa7cdc6969
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2025-06-04 03:45:11 +1400

    Set health after successfully attacked

--- src/main/java/io/wdsj/hybridfix/mixin/late/witchery/util/EntityUtilMixin.java
+++ src/main/java/io/wdsj/hybridfix/mixin/late/witchery/util/EntityUtilMixin.java
@@ -1,6 +1,9 @@
 package io.wdsj.hybridfix.mixin.late.witchery.util;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.DamageSource;
 import net.msrandom.witchery.util.EntityUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public abstract class EntityUtilMixin {
     @Redirect(
             method = "instantDeath",
-            at =  @At(
+            at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/entity/EntityLivingBase;setHealth(F)V",
                     remap = true
@@ -23,5 +26,21 @@ public abstract class EntityUtilMixin {
     )
     private static void swallowSetHealth(EntityLivingBase instance, float health) {
         // no-op
+    }
+
+    /* Prevent NaN health */
+    @WrapOperation(
+            method = "instantDeath",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/EntityLivingBase;attackEntityFrom(Lnet/minecraft/util/DamageSource;F)Z",
+                    remap = true
+            ),
+            remap = false
+    )
+    private static boolean wrapAttackEntityFrom(EntityLivingBase instance, DamageSource damageSource, float damage, Operation<Boolean> original) {
+        boolean result = original.call(instance, damageSource, damage);
+        if (result) instance.setHealth(0.0F);
+        return result;
     }
 }
