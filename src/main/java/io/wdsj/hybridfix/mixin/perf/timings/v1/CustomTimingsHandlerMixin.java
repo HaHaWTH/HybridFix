02299106dddd8ce393c99091f333b246683c2d38
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2025-07-08 00:09:39 +1400

    Rewrite Timings remover

--- src/main/java/io/wdsj/hybridfix/mixin/perf/timings/v1/CustomTimingsHandlerMixin.java
+++ src/main/java/io/wdsj/hybridfix/mixin/perf/timings/v1/CustomTimingsHandlerMixin.java
@@ -2,9 +2,9 @@ package io.wdsj.hybridfix.mixin.perf.timings.v1;
 
 import org.spigotmc.CustomTimingsHandler;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.PrintStream;
@@ -12,16 +12,17 @@ import java.util.Queue;
 
 @Mixin(CustomTimingsHandler.class)
 public abstract class CustomTimingsHandlerMixin {
-
-    @Shadow(remap = false) private static Queue<CustomTimingsHandler> HANDLERS;
-
-    @Inject(
+    @Redirect(
             method = "<init>(Ljava/lang/String;Lorg/spigotmc/CustomTimingsHandler;)V",
-            at = @At("RETURN"),
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/Queue;add(Ljava/lang/Object;)Z"
+            ),
             remap = false
     )
-    private void init(String name, CustomTimingsHandler parent, CallbackInfo ci) {
-        HANDLERS.poll();
+    private boolean init(Queue<CustomTimingsHandler> instance, Object e) {
+        // no-op
+        return false;
     }
 
     @Inject(
