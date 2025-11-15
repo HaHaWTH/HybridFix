70b9d3a1323092c9f3664932fed8651b2725ea9d
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2025-11-15 21:23:36 +0800

    Change injection point

--- src/main/java/io/wdsj/hybridfix/mixin/fix/chunk_system/CraftWorldMixin.java
+++ src/main/java/io/wdsj/hybridfix/mixin/fix/chunk_system/CraftWorldMixin.java
@@ -1,30 +1,32 @@
 package io.wdsj.hybridfix.mixin.fix.chunk_system;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.ChunkProviderServer;
 import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CraftWorld.class)
 public abstract class CraftWorldMixin {
     /**
      * Preventing unloading Forge forced chunks from Bukkit side.
      */
-    @Inject(
+    @WrapOperation(
             method = "unloadChunk0",
-            at = @At("RETURN"),
-            cancellable = true,
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/gen/ChunkProviderServer;unloadChunk(Lnet/minecraft/world/chunk/Chunk;Z)Z"
+            ),
             remap = false
     )
-    public void unloadChunk0(int x, int z, boolean save, CallbackInfoReturnable<Boolean> cir, @Local(ordinal = 0) Chunk chunk) {
-        if (chunk == null) return;
+    public boolean unloadChunk0(ChunkProviderServer instance, Chunk chunk, boolean b, Operation<Boolean> original) {
         World world = ((CraftWorld) (Object) this).getHandle();
         if (world.getPersistentChunks().containsKey(chunk.getPos())) {
-            cir.setReturnValue(false);
+            return false;
         }
+        return original.call(instance, chunk, b);
     }
 }
