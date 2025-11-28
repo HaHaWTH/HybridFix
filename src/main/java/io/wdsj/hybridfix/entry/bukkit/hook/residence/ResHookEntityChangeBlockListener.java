dcee1b6fd4fe19e8e1c565d84d789c022ea8ae1f
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2025-11-29 00:34:41 +0800

    Process for fake player

--- src/main/java/io/wdsj/hybridfix/entry/bukkit/hook/residence/ResHookEntityChangeBlockListener.java
+++ src/main/java/io/wdsj/hybridfix/entry/bukkit/hook/residence/ResHookEntityChangeBlockListener.java
@@ -2,9 +2,11 @@ package io.wdsj.hybridfix.entry.bukkit.hook.residence;
 
 import com.bekvon.bukkit.residence.Residence;
 import com.bekvon.bukkit.residence.containers.Flags;
+import com.bekvon.bukkit.residence.listeners.ResidenceBlockListener;
 import com.bekvon.bukkit.residence.protection.FlagPermissions;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -22,6 +24,14 @@ public class ResHookEntityChangeBlockListener implements Listener {
         EntityType type = entity.getType();
         if (type == EntityType.ENDERMAN || type == EntityType.WITHER) return;
         // End
+
+        if (entity instanceof Player) {
+            Player player = (Player) entity;
+            if (!ResidenceBlockListener.canBreakBlock(player, event.getBlock().getLocation(), false)) {
+                event.setCancelled(true);
+            }
+            return;
+        }
 
         FlagPermissions perms = plugin.getPermsByLoc(event.getBlock().getLocation());
         if (!perms.has(Flags.destroy, true)) {
