99ed9418479c1bcaf86f28cc98f3e985b3ba557d
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2025-12-24 22:44:31 +0800

    Fix jar discovery process for MRJAR

---
+++ src/main/java/io/wdsj/hybridfix/mixin/base/patch/forge/JarDiscovererMixin.java
@@ -0,0 +1,29 @@
+package io.wdsj.hybridfix.mixin.base.patch.forge;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraftforge.fml.common.discovery.JarDiscoverer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.regex.Matcher;
+import java.util.zip.ZipEntry;
+
+@SuppressWarnings("LocalMayUseName")
+@Mixin(value = JarDiscoverer.class, remap = false)
+public class JarDiscovererMixin {
+    @WrapOperation(
+            method = "findClassesASM",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/regex/Matcher;matches()Z"
+            )
+    )
+    private boolean ignoreMultiReleaseClasses(Matcher instance, Operation<Boolean> original, @Local(ordinal = 0) ZipEntry ze) {
+        if (ze.getName().contains("META-INF/versions")) {
+            return false;
+        }
+        return original.call(instance);
+    }
+}
