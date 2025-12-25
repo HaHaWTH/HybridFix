142b410195c05c04beb2d0430d6d3ce8501683ba
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2025-12-25 21:38:30 +0800

    Check for module-info classes

--- src/main/java/io/wdsj/hybridfix/mixin/base/patch/forge/JarDiscovererMixin.java
+++ src/main/java/io/wdsj/hybridfix/mixin/base/patch/forge/JarDiscovererMixin.java
@@ -5,6 +5,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraftforge.fml.common.discovery.JarDiscoverer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.regex.Matcher;
@@ -20,10 +21,16 @@ public class JarDiscovererMixin {
                     target = "Ljava/util/regex/Matcher;matches()Z"
             )
     )
-    private boolean ignoreMultiReleaseClasses(Matcher instance, Operation<Boolean> original, @Local(ordinal = 0) ZipEntry ze) {
-        if (ze.getName().startsWith("META-INF/versions")) {
+    private boolean ignoreClasses(Matcher instance, Operation<Boolean> original, @Local(ordinal = 0) ZipEntry ze) {
+        return original.call(instance) && hybridFix$isValidClass(ze);
+    }
+
+    @Unique
+    private static boolean hybridFix$isValidClass(ZipEntry ze) {
+        String name = ze.getName();
+        if (name.equals("module-info.class") || name.endsWith("/module-info.class")) {
             return false;
         }
-        return original.call(instance);
+        return !name.startsWith("META-INF/versions/");
     }
 }
