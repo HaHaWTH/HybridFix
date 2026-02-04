612ee6772ff66135a7b049219a12dc576abe111e
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2026-02-04 17:14:12 +0800

    Remove incompatibility notes

--- src/main/java/io/wdsj/hybridfix/config/Settings.java
+++ src/main/java/io/wdsj/hybridfix/config/Settings.java
@@ -107,7 +107,7 @@ public class Settings {
     @Config.RequiresMcRestart
     public static boolean invertFakePlayerBlacklist = false;
 
-    @Config.Comment("(Server) More Bukkit API implementation from Paper and modern versions.\n(Incompatible with: Quark)")
+    @Config.Comment("(Server) More Bukkit API implementation from Paper and modern versions.")
     @Config.Name("Extra Bukkit API")
     @Config.RequiresMcRestart
     public static boolean extraBukkitApi = false;
@@ -132,7 +132,7 @@ public class Settings {
     @Config.RequiresMcRestart
     public static boolean compatModeForAttackBridge = false;
 
-    @Config.Comment("(Server) Fixes for Forge and Bukkit chunk systems.\n(Incompatible with: Quark)")
+    @Config.Comment("(Server) Fixes for Forge and Bukkit chunk systems.")
     @Config.Name("Fixes for Forge and Bukkit chunk systems")
     @Config.RequiresMcRestart
     public static boolean fixesForForgeAndBukkitChunkSystems = false;
