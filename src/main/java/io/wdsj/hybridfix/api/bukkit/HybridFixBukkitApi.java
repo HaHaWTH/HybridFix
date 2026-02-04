c845df9d5c22e05bd6b9db70d968dc086f24c2bc
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2026-02-04 14:21:29 +0800

    Full data sync utils

--- src/main/java/io/wdsj/hybridfix/api/bukkit/HybridFixBukkitApi.java
+++ src/main/java/io/wdsj/hybridfix/api/bukkit/HybridFixBukkitApi.java
@@ -1,9 +1,21 @@
 package io.wdsj.hybridfix.api.bukkit;
 
+import io.wdsj.hybridfix.util.SneakyThrow;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.JsonToNBT;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.PlayerList;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.util.FakePlayer;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftEntity;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
+@SuppressWarnings("unused")
 public class HybridFixBukkitApi {
     protected HybridFixBukkitApi() {
     }
@@ -24,5 +36,46 @@ public class HybridFixBukkitApi {
      */
     public boolean isFakePlayer(Player player) {
         return ((CraftEntity) player).getHandle() instanceof FakePlayer;
+    }
+
+    /**
+     * Serializes the data of a player to string.
+     * Works by saving player data to disk and re-reading it.
+     *
+     * @param player The player to serialize.
+     * @return The serialized data of the player.
+     */
+    @ApiStatus.Experimental
+    public static String serializePlayerData(@NotNull Player player) {
+        EntityPlayerMP serverPlayer = (EntityPlayerMP) ((CraftEntity) player).getHandle();
+        WorldServer serverLevel = serverPlayer.getServerWorld();
+        MinecraftServer server = serverLevel.getMinecraftServer();
+        Objects.requireNonNull(server, "Server is null");
+        PlayerList playerList = server.getPlayerList();
+        playerList.playerDataManager.writePlayerData(serverPlayer);
+        return Objects.requireNonNull(playerList.playerDataManager.readPlayerData(serverPlayer)).toString();
+    }
+
+    /**
+     * Deserializes the data of a player from string.
+     *
+     * @param player The player to deserialize.
+     * @param data   The serialized data of the player.
+     */
+    @ApiStatus.Experimental
+    public static void deserializePlayerData(@NotNull Player player, @NotNull String data) {
+        try {
+            EntityPlayerMP serverPlayer = (EntityPlayerMP) ((CraftEntity) player).getHandle();
+            WorldServer serverLevel = serverPlayer.getServerWorld();
+            MinecraftServer server = serverLevel.getMinecraftServer();
+            Objects.requireNonNull(server, "Server is null");
+            PlayerList playerList = server.getPlayerList();
+            NBTTagCompound nbt = JsonToNBT.getTagFromJson(data);
+            serverPlayer.readFromNBT(nbt);
+            playerList.playerDataManager.writePlayerData(serverPlayer);
+            playerList.playerDataManager.readPlayerData(serverPlayer);
+        } catch (Exception e) {
+            SneakyThrow.sneaky(e);
+        }
     }
 }
