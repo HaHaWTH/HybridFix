02bb83264b92ae132c5080aa62eae6a0f8cceca7
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2026-02-09 17:14:27 +0800

    throw when main check failed

--- src/main/java/io/wdsj/hybridfix/util/TickThread.java
+++ src/main/java/io/wdsj/hybridfix/util/TickThread.java
@@ -8,6 +8,7 @@ public final class TickThread {
     public static void ensureTickThread(final String reason) {
         if (!isTickThread()) {
             LOGGER.error("Thread failed main thread check: {}, context={}", reason, new Throwable());
+            throw new IllegalStateException(reason);
         }
     }
 
