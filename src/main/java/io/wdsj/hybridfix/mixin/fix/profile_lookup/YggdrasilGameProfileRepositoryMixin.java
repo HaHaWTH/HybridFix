fb6093135d8fea833a9b7622a0ff994d59ea5a7c
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2026-02-20 20:13:19 +0800

    [ci skip] include ref

--- src/main/java/io/wdsj/hybridfix/mixin/fix/profile_lookup/YggdrasilGameProfileRepositoryMixin.java
+++ src/main/java/io/wdsj/hybridfix/mixin/fix/profile_lookup/YggdrasilGameProfileRepositoryMixin.java
@@ -10,6 +10,43 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
+/*
+ * Fixes NPE when profile lookup fails.
+ * Reference crash report:
+java.lang.NullPointerException: Cannot read the array length because the return value of "com.mojang.authlib.yggdrasil.response.ProfileSearchResultsResponse.getProfiles()" is null
+    at com.mojang.authlib.yggdrasil.YggdrasilGameProfileRepository.findProfilesByNames(YggdrasilGameProfileRepository.java:57)
+    at net.minecraft.server.management.PlayerProfileCache.lookupProfile(PlayerProfileCache.java:100)
+    at net.minecraft.server.management.PlayerProfileCache.getGameProfileForUsername(PlayerProfileCache.java:177)
+    at codechicken.lib.util.ServerUtils.getGameProfile(ServerUtils.java:65)
+    at codechicken.lib.util.ServerUtils.isPlayerOP(ServerUtils.java:74)
+    at codechicken.chunkloader.manager.ChunkLoaderManager.allowOffline(ChunkLoaderManager.java:578)
+    at codechicken.chunkloader.manager.OrganiserStorage$SavedData.load(OrganiserStorage.java:187)
+    at codechicken.chunkloader.manager.ChunkLoaderManager.load(ChunkLoaderManager.java:540)
+    at codechicken.chunkloader.handler.ChunkLoaderEventHandler.onChunkDataLoad(ChunkLoaderEventHandler.java:44)
+    at net.minecraftforge.fml.common.eventhandler.ASMEventHandler_2548_ChunkLoaderEventHandler_onChunkDataLoad_Load.invoke(.dynamic)
+    at net.minecraftforge.fml.common.eventhandler.ASMEventHandler.invoke(ASMEventHandler.java:90)
+    at net.minecraftforge.fml.common.eventhandler.EventBus.post(EventBus.java:190)
+    at net.minecraftforge.common.chunkio.ChunkIOProvider.syncCallback(ChunkIOProvider.java:103)
+    at net.minecraftforge.common.chunkio.ChunkIOExecutor.syncChunkLoad(ChunkIOExecutor.java:98)
+    at net.minecraft.world.gen.ChunkProviderServer.loadChunk(ChunkProviderServer.java:126)
+    at net.minecraft.world.gen.ChunkProviderServer.loadChunk(ChunkProviderServer.java:96)
+    at net.minecraft.world.gen.ChunkProviderServer.provideChunk(ChunkProviderServer.java:143)
+    at net.minecraft.world.World.getChunk(World.java:444)
+    at net.minecraft.world.World.getChunk(World.java:439)
+    at net.minecraft.world.World.getTopSolidOrLiquidBlock(World.java:1871)
+    at net.minecraft.world.WorldProvider.getRandomizedSpawnPoint(WorldProvider.java:351)
+    at net.minecraft.entity.player.EntityPlayerMP.<init>(EntityPlayerMP.java:193)
+    at net.minecraftforge.common.util.FakePlayer.<init>(FakePlayer.java:45)
+    at net.minecraftforge.common.util.FakePlayerFactory.get(FakePlayerFactory.java:74)
+    at blusunrize.immersiveengineering.common.util.FakePlayerUtil.onLoad(FakePlayerUtil.java:61)
+    at net.minecraftforge.fml.common.eventhandler.ASMEventHandler_671_FakePlayerUtil_onLoad_Load.invoke(.dynamic)
+    at net.minecraftforge.fml.common.eventhandler.ASMEventHandler.invoke(ASMEventHandler.java:90)
+    at net.minecraftforge.fml.common.eventhandler.EventBus.post(EventBus.java:190)
+    at net.minecraft.server.MinecraftServer.loadAllWorlds(MinecraftServer.java:382)
+    at net.minecraft.server.dedicated.DedicatedServer.init(DedicatedServer.java:335)
+    at net.minecraft.server.MinecraftServer.run(MinecraftServer.java:647)
+    at java.lang.Thread.run(Thread.java:1583)
+*/
 @Mixin(value = YggdrasilGameProfileRepository.class, remap = false)
 public abstract class YggdrasilGameProfileRepositoryMixin {
     @Unique
